cf047c6ba2e36294a6bcdf520abe925543970e6f
Peter Karth <falonofthetower@gmail.com>
2016-12-14 04:05:14 -0500

    ActionMailer settings

--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,18 @@
 # Load the Rails application.
 require_relative 'application'
 
-Rails.application.routes.default_url_options[:host] = ENV["HOST"]
 # Initialize the Rails application.
 Rails.application.initialize!
+
+Rails.application.routes.default_url_options[:host] = ENV["HOST"]
+
+ActionMailer::Base.smtp_settings = {
+  :user_name => ENV['SENDGRID_USERNAME'],
+  :password => ENV['SENDGRID_PASSWORD'],
+  :domain => ENV['HOST'],
+  :address => 'smtp.sendgrid.net',
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
+ActionMailer::Base.delivery_method = :smtp
